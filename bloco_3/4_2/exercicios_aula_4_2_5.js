7f1b4576058a4fc93f43997c73d4042cbf62d367
Leonardo <lecastro16@gmail.com>
2020-08-28 18:33:03 -0300

    Exercicio 8 - criando valors de 1 a 25 e imprimindo

--- bloco_3/4_2/exercicios_aula_4_2_5.js
+++ bloco_3/4_2/exercicios_aula_4_2_5.js
@@ -1,7 +1,7 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let maiorValor = 0;
 
-for (i=0; i<numbers.length; i+=1){
+for (let i=0; i<numbers.length; i+=1){
     if (numbers[i]>maiorValor){
         maiorValor=numbers[i];
     }
--- bloco_3/4_2/exercicios_aula_4_2_6.js
+++ bloco_3/4_2/exercicios_aula_4_2_6.js
@@ -1,7 +1,7 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let quantidadeDeImpares = 0;
 
-for (i=0; i<numbers.length; i+=1){
+for (let i=0; i<numbers.length; i+=1){
     if (numbers[i]%2!=0){
         quantidadeDeImpares=quantidadeDeImpares+1;
     }
