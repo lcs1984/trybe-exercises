15b3ff34863883c7ab19e82bb9e1ee7f858575b4
Leonardo <lecastro16@gmail.com>
2020-09-08 19:06:46 -0300

    commit inicial exemplos do video

--- bloco_3/4_4/arquivo_de_teste.js
+++ bloco_3/4_4/arquivo_de_teste.js
@@ -27,26 +27,56 @@
 
    //console.log(novaString);
    //return novaString;
-   let recebeString = " Leonardo "
-   let posicaoDoEspaco = 0;
-   let palavraCortada;
-   let arrayParaImprimir = [];
+  //  let recebeString = " Leonardo "
+  //  let posicaoDoEspaco = 0;
+  //  let palavraCortada;
+  //  let arrayParaImprimir = [];
  
-   for (let i = 0; i < recebeString.length; i += 1) {
-    if (recebeString[0]===" "){
-        i += 1; 
-    } else if (recebeString[recebeString.length-1]===" "){
-        recebeString = recebeString.slice(0,recebeString.length-1);
-    } 
+  //  for (let i = 0; i < recebeString.length; i += 1) {
+  //   if (recebeString[0]===" "){
+  //       i += 1; 
+  //   } else if (recebeString[recebeString.length-1]===" "){
+  //       recebeString = recebeString.slice(0,recebeString.length-1);
+  //   } 
     
-    if (recebeString[i] === " ") {
-       palavraCortada = recebeString.slice(posicaoDoEspaco, i);
-       arrayParaImprimir.push(palavraCortada);
-       posicaoDoEspaco = i;
-     }
-   }
-   palavraCortada = recebeString.slice(posicaoDoEspaco, recebeString.length);
-   arrayParaImprimir.push(palavraCortada);
+  //   if (recebeString[i] === " ") {
+  //      palavraCortada = recebeString.slice(posicaoDoEspaco, i);
+  //      arrayParaImprimir.push(palavraCortada);
+  //      posicaoDoEspaco = i;
+  //    }
+  //  }
+  //  palavraCortada = recebeString.slice(posicaoDoEspaco, recebeString.length);
+  //  arrayParaImprimir.push(palavraCortada);
  
-   //return arrayParaImprimir;
-   console.log(arrayParaImprimir);
+  //  //return arrayParaImprimir;
+  //  console.log(arrayParaImprimir);
+
+  // let nomesDeTecnologia ={
+  //   tech:"",
+  //   name: ""
+  //   };
+
+// seu código aqui
+let conjuntoDeNumeros = [1,1,1,9,3,5,2,0,6,7,2];
+  // seu código aqui
+  let maximo = 0;
+  
+  if (conjuntoDeNumeros.length!==11){
+    console.log("Array com tamanho incorreto");
+  } 
+  
+  for (let i= 0; i<conjuntoDeNumeros.length; i +=1){
+    if (conjuntoDeNumeros[i]<0 || conjuntoDeNumeros>9){
+      console.log("não é possível gerar um número de telefone com esses valores");
+    }
+  }
+
+    for (let i = 0; i<conjuntoDeNumeros.length; i +=1){
+      for (let j = 0; j<conjuntoDeNumeros.length; j +=1){
+        if (conjuntoDeNumeros[i]===conjuntoDeNumeros[j]){
+          maximo += 1; 
+        }
+      }
+    }    
+
+   
