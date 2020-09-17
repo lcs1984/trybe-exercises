b4c346e2e387add0085e1df3710d94124ea476aa
Leonardo <lecastro16@gmail.com>
2020-09-17 18:32:17 -0300

    organizei as pastas dos blocos 4 e 5

---
+++ Bloco_4/4_4/exercicios_aula_4_1_3_P2.js
@@ -0,0 +1,12 @@
+function menorValor(arrayDeInteiros) {
+  let menorNumero = arrayDeInteiros[0];
+
+  for (let i = 0; i <= arrayDeInteiros.length; i += 1) {
+    if (arrayDeInteiros[i] < menorNumero) {
+      maiorNumero = arrayDeInteiros[i];
+    } else {
+    }
+  }
+  console.log(maiorNumero);
+  return maiorNumero;
+}
