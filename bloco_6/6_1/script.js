c738d90bdd7f4ff5dd4c220775a99ba0afb313a9
Leonardo <lecastro16@gmail.com>
2020-09-22 19:07:13 -0300

    preenchendo formulario

--- bloco_6/6_1/script.js
+++ bloco_6/6_1/script.js
@@ -40,13 +40,13 @@ for (index = 0; index < listaDeEstadosDoBrasil.length; index += 1) {
 function verificaFormatoDaData() {
   let data = document.querySelector("#data-de-inicio").value;
   data = data.split("/");
-  if (data[0]<0 || data[0] > 31){
+  if (data[0] < 0 || data[0] > 31) {
     let erroDia = "O dia precisa estar entre 0 e 31";
-    console.log(erroDia)
-  } else if (data[1]<0 || data[1]>12) {
+    console.log(erroDia);
+  } else if (data[1] < 0 || data[1] > 12) {
     let erroMes = "O mês precisa estar entre 0 e 12";
     console.log(erroMes);
-  } else if (data [2]<0){
+  } else if (data[2] < 0) {
     let erroAno = "O ano precia ser positivo";
     console.log(erroAno);
   }
@@ -57,7 +57,29 @@ function verificaFormatoDaData() {
 
 let enviar = document.querySelector(".form-do-exercicio");
 
-enviar.addEventListener("submit", function(event){
+enviar.addEventListener("submit", function (event) {
   event.preventDefault();
   verificaFormatoDaData();
+  carregaDadosDoFormulario();
 });
+
+function carregaDadosDoFormulario () {
+let nomeCompleto = document.getElementById("nome-completo").value;
+console.log(nomeCompleto)
+let email = document.getElementById("e-mail").value;
+let cpf = document.getElementById("cpf").value;
+let endereco = document.getElementById("endereco").value;
+let cidade = document.getElementById("cidade").value;
+let estado = document.getElementById("estados-do-brasil").value;
+console.log(estado);
+let tipoDeMoradia = document.getElementsByName("casa-apto").value;
+console.log(tipoDeMoradia);
+let resumoDoCurriculo = document.getElementById("curriculo").value;
+console.log(resumoDoCurriculo);
+let cargo = document.getElementById("cargo").value;
+let descricaoDoCargo = document.getElementById("descricao-do-cargo").value;
+console.log(descricaoDoCargo);
+let dataDeInicio = document.getElementById("data-de-inicio").value;
+console.log(dataDeInicio);
+console.log(dataDeInicio);
+};
