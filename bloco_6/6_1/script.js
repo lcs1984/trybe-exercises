17b57e5aca6d1212171c5cb3468ea1efd3f5d2d1
Leonardo <lecastro16@gmail.com>
2020-09-22 14:30:52 -0300

    ajustando o javascript

--- bloco_6/6_1/script.js
+++ bloco_6/6_1/script.js
@@ -37,9 +37,27 @@ for (index = 0; index < listaDeEstadosDoBrasil.length; index += 1) {
   estadosDoBrasil.appendChild(optionList);
 }
 
-//function verificaFormatoDaData () {
-let data = document.querySelector("#data-de-inicio");
-//data = data.split("/");
-console.log(data.innerText);
-//console.log(data[0]);
-//}
+function verificaFormatoDaData() {
+  let data = document.querySelector("#data-de-inicio").value;
+  data = data.split("/");
+  if (data[0]<0 || data[0] > 31){
+    let erroDia = "O dia precisa estar entre 0 e 31";
+    console.log(erroDia)
+  } else if (data[1]<0 || data[1]>12) {
+    let erroMes = "O mês precisa estar entre 0 e 12";
+    console.log(erroMes);
+  } else if (data [2]<0){
+    let erroAno = "O ano precia ser positivo";
+    console.log(erroAno);
+  }
+  console.log(data[0]);
+  console.log(data[1]);
+  console.log(data[2]);
+}
+
+let enviar = document.querySelector(".form-do-exercicio");
+
+enviar.addEventListener("submit", function(event){
+  event.preventDefault();
+  verificaFormatoDaData();
+});
