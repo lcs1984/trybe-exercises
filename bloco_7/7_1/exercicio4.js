3360c91ace5618f6f3fb77314aca7ffcf74d837c
Leonardo <lecastro16@gmail.com>
2020-09-28 17:19:45 -0300

    alguns exemplos da aula

---
+++ bloco_7/7_1/exercicio4.js
@@ -0,0 +1,11 @@
+let nome = "Leonardo de Castro x Sardinha";
+nome[5]="trybe";
+console.log(nome);
+const parametro = "Trybe";
+console.log(`${nome.length}`);
+
+for (index = 0; index < nome.length; index += 1) {
+    if (nome[index]=="x"){
+
+    }
+}
