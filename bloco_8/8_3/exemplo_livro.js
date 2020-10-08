5820216451c62485e1f41f6e765b7037cb6da7ea
Leonardo <lecastro16@gmail.com>
2020-10-08 15:30:23 -0300

    exercicios iniciais

--- bloco_8/8_3/exemplo_livro.js
+++ bloco_8/8_3/exemplo_livro.js
@@ -1,10 +1,11 @@
 const a = [1, 2, 3, 4];
 
+
 const sum = a.reduce(function (x, y) {
   return x + y;
 });
 
-const sumb = a.reduce((x, y) => x + y);
+const sumb = a.reduce((x, y) => x + y, 10);
 
 const mult = a.reduce(function (x, y) {
   return x * y;
