504bcf8adeb71f8cc2eddee0a723bd2112e414ab
Leonardo <lecastro16@gmail.com>
2020-10-21 15:26:42 -0300

    adicionando algumas caracteristicas do projeto zoo

--- bloco_8/8_1/exemplo_aula2.js
+++ bloco_8/8_1/exemplo_aula2.js
@@ -9,7 +9,10 @@
 
 // console.log(firstMultipleOf5);
 
+//*** DIFERENTEMENTE DA FOREACH A FIND RETORNA UM VALOR  */
+//** A FUNÇÃO FOREACH APENAS PERCORRE O ARRAY EXECUTANDO UM DETERMINADO COMANDO OU FUNÇÃO */
 const numbers = [11, 24, 39, 47, 50, 62, 75, 81, 96, 100];
+const letras = ["E", "D", "C", "B", "A"]
 const firstMultipleOf5 = numbers.find((number) => number % 5 === 0);
 
 console.log(firstMultipleOf5);
@@ -17,5 +20,7 @@ console.log(firstMultipleOf5);
 numbers.find((number) => number >= 50);
 console.log(numbers.filter((number)=> number>=50));
 numbers.forEach((number)=>console.log(number));
-
+// A funçao Sort pode ser com parâmetro ou não....sem paramentros funciona só para strings
+console.log(numbers.sort((maior, menor)=>maior-menor));
+console.log(letras.sort());
 
