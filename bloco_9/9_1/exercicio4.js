da674eb7fa1c0d7c8d3c49ed0023c983b964d1c2
Leonardo <lecastro16@gmail.com>
2020-10-22 16:38:18 -0300

    todos os exemplos de aula v1

--- bloco_9/9_1/exercicio4.js
+++ bloco_9/9_1/exercicio4.js
@@ -11,7 +11,9 @@ const getMarsTemperature = () => {
   return Math.floor(Math.random() * maxTemperature);
 };
 
-// crie a função sendMarsTemperature abaixo
-
+const sendMarsTemperature = () => {
+  const currentTemperature = getMarsTemperature();
+  setTimeout(() => console.log(`Mars temperature is: ${currentTemperature} degree Celsius`), messageDelay());
+};
 
 sendMarsTemperature(); // imprime "Mars temperature is: 20 degree Celsius", por exemplo
