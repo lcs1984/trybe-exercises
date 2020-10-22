85ea323d51769cbdcfd66100ba2e2dbf0e2759f1
Leonardo <lecastro16@gmail.com>
2020-10-22 16:35:29 -0300

    feitos os exercicios 1 ao 4...falta o 5 e 6

--- bloco_9/9_1/exercicio4.js
+++ bloco_9/9_1/exercicio4.js
@@ -13,5 +13,9 @@ const getMarsTemperature = () => {
 
 // crie a função sendMarsTemperature abaixo
 
+const sendMarsTemperature = () => {
+  const currentTemperature = getMarsTemperature();
+  setTimeout(()=> console.log(`Mars temperature is: ${currentTemperature} degree Celsius`), messageDelay());
+}
 
 sendMarsTemperature(); // imprime "Mars temperature is: 20 degree Celsius", por exemplo
