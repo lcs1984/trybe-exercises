633ff8a1a73423d4963e16462aa48d41b174028d
Leonardo <lecastro16@gmail.com>
2020-11-09 18:36:57 -0300

    faltou completar o exercicio final tasks

--- bloco_11/11_1/task-list/src/App.js
+++ bloco_11/11_1/task-list/src/App.js
@@ -1,25 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const task = (value)=> {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <li>{value}</li>
   );
+}
+
+function App() {
+  return (task);
 }
 
 export default App;
