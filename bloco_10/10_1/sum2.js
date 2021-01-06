866b2fff8c9f2b896ffdf90158c915c22729f4d4
Leonardo <lecastro16@gmail.com>
2021-01-06 15:37:12 -0300

    modificando exercicios sum2 Turma 8

--- bloco_10/10_1/sum2.js
+++ bloco_10/10_1/sum2.js
@@ -1,3 +1,12 @@
 const sum2 = (a, b) => a + b;
 
 module.exports = sum2;
+
+const SumNumbers = (a, b, callback) => {
+    setTimeout(() => {
+      const result = a + b;
+      callback(result);
+    }, 500)
+  }
+
+  SumNumbers(1,2);
