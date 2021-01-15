0cd293ada817103f261624ba10e14e9d757c676d
Leonardo <lecastro16@gmail.com>
2021-01-15 16:15:04 -0300

    finalizando exemplo do alberto

--- bloco_12/12_2/exemplo-aula-alberto/src/EstadoFavorito.js
+++ bloco_12/12_2/exemplo-aula-alberto/src/EstadoFavorito.js
@@ -2,17 +2,19 @@ import React from "react";
 
 class EstadoFavorito extends React.Component {
   render() {
-      const {value, handleChange} = this.props;
-      console.log(`estou no filho com o valor ${value}`);
-      return (
-        <label>
-        <textarea
-          name="estadoFavorito"
-          value={value}
-          onChange={handleChange}
-        />
+    const { value, handleChange } = this.props;
+
+    console.log(`estou no filho com o valor ${value}`);
+
+    let error = undefined;
+    if (value.length > 120) error = "texto muito grande!";
+
+    return (
+      <label>
+        <textarea name="estadoFavorito" value={value} onChange={handleChange} />
+        <div>{error ? error : ""}</div>
       </label>
-      )
+    );
   }
 }
 
