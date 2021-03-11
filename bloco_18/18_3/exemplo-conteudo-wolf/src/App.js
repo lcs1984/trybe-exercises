268f3792182bd8ea5f737c7f124f05273d22cfc0
Leonardo <lecastro16@gmail.com>
2021-03-11 18:11:16 -0300

    finalizando exemplo

--- bloco_18/18_3/exemplo-conteudo-wolf/src/App.js
+++ bloco_18/18_3/exemplo-conteudo-wolf/src/App.js
@@ -1,19 +1,33 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 
 function App() {
-  const [pokemons, setPokemons] = useState([])
-  const url = "https://pokeapi.co/api/v2/pokemon?limit=151"
-  
+  const [pokemons, setPokemons] = useState([]);
+  const [limit, setLimit] = useState(10);
+  const url = `https://pokeapi.co/api/v2/pokemon?limit=${limit}`;
+
   // componentDidMount
-  // use
-  useEffect(async() => {
-    const {results} = await fetch(url).then(response=>response.json());
+  // useEffect(callback, [array de dependências])
+  // se eu não passar nenhum parametro para o useEffet, ele atualiza as variaves ou seja, 
+  // comporta-se como um componentDidupdate
+  // Se eu passar um array vazio, ele funciona como o componentDidMount
+  useEffect(async () => {
+    const { results } = await fetch(url).then((response) => response.json());
     setPokemons(results);
-  })
+  }, [limit]);
+
+  const handleMorePokemons = () => {
+    setLimit(limit+10);
+  };
+
   return (
-  <ul>
-    {pokemons.map(item => <li key={item.name}>{item.name}</li>)}
-  </ul>
+    <div>
+      <button type='button' onClick={handleMorePokemons}>Buscar mais Pokemons</button>
+      <ul>
+        {pokemons.map((item) => (
+        <li key={item.name}>{item.name}</li>
+         ))}
+      </ul>
+    </div>
   );
 }
 
