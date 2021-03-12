e6ba2e15ab076ea0a71db782c15e11f5bc10d1de
Leonardo <lecastro16@gmail.com>
2021-03-12 10:20:28 -0300

    finalizando o exemplo do conteudo video 1

--- bloco_18/18_1/exemplo-conteudo-wolf/src/components/Daugther.js
+++ bloco_18/18_1/exemplo-conteudo-wolf/src/components/Daugther.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import MyContext from './MyContext';
 
 class Daugther extends React.Component {
-
   render() {
     return (
-      <div>
-        <h4>Eu sou a filha</h4>
-      </div>
+      <MyContext.Consumer>
+        {(value) => (
+          <div>
+            <p>Eu tenho ${value.money} pra gastar</p>
+            <button onClick={value.spendMoney}>Gastar</button>
+          </div>
+        )}
+      </MyContext.Consumer>
     );
   }
 }
--- bloco_18/18_1/exemplo-conteudo-wolf/src/components/GreatGrandMother.js
+++ bloco_18/18_1/exemplo-conteudo-wolf/src/components/GreatGrandMother.js
@@ -1,14 +1,32 @@
 import React from 'react';
 import GrandMother from './GrandMother';
+import MyContext from './MyContext';
 
 class GreatGrandMother extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      money: 1000000000,
+    };
+    this.handleSpendMoney = this.handleSpendMoney.bind(this);
+  }
+
+  handleSpendMoney() {
+    this.setState((previousState) => ({ money: previousState.money - 100000 }));
+  }
 
   render() {
+    const contextValue = {
+      money: this.state.money,
+      spendMoney: this.handleSpendMoney,
+    };
     return (
-      <div>
-        <h1>Eu sou a bisavó</h1>
-        <GrandMother />
-      </div>
+      <MyContext.Provider value={ contextValue }>
+        <div>
+          <h1>Eu sou a bisavó</h1>
+          <GrandMother />
+        </div>
+      </MyContext.Provider>
     );
   }
 }
