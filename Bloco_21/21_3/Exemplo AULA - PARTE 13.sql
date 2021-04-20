2edae52ac50fcc9fbc1401dfa83e1170d0b7663b
Leonardo <lecastro16@gmail.com>
2021-04-20 12:01:13 -0300

    iniciando os exercicios exemplos de aula

---
+++ Bloco_21/21_3/Exemplo AULA - PARTE 13.sql
@@ -0,0 +1,26 @@
+-- SELECT * FROM sakila.film_actor;
+
+-- SELECT COUNT(*) AS total_film, fa.actor_id, CONCAT(a.first_name, ' ', a.last_name) AS Full_Name
+-- FROM sakila.film_actor AS fa
+-- INNER JOIN sakila.actor AS a
+-- ON fa.actor_id = a.actor_id
+-- GROUP BY actor_id
+-- ORDER BY total_film DESC
+-- LIMIT 10;
+
+USE sakila;
+DELIMITER $$
+CREATE PROCEDURE MostrarOsDezAtoresMaisPopulares()
+BEGIN
+	SELECT COUNT(*) AS total_film, fa.actor_id, CONCAT(a.first_name, ' ', a.last_name) AS Full_Name
+	FROM sakila.film_actor AS fa
+	INNER JOIN sakila.actor AS a
+	ON fa.actor_id = a.actor_id
+	GROUP BY actor_id
+	ORDER BY total_film DESC
+	LIMIT 10;
+END
+
+$$ DELIMITER ;
+
+CALL MostrarOsDezAtoresMaisPopulares();
