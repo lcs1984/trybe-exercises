a7b4fa1acd9f4d335f04ca04fdaad68bf3ace973
Leonardo <lecastro16@gmail.com>
2021-05-18 15:08:56 -0300

    exercicio fatorial v0

--- Bloco_26/todos_os_scripts/fatorial.js
+++ Bloco_26/todos_os_scripts/fatorial.js
@@ -3,18 +3,18 @@ const readline = require("readline-sync");
 function fatorial(x) {
   if (x == 0) {
     return 1;
-  } else if(x<0) {
-      return console.log('digite um número inteiro maior ou igual a zero');
-  } 
-  else {
+  } else if (x < 0) {
+    return console.log("digite um número inteiro maior ou igual a zero");
+  } else {
     return x * fatorial(x - 1);
   }
 }
 
 function roda_fatorial() {
-    const x = readline.questionInt('Digite um número inteiro: ');
-    const resultado = fatorial(x);
-    return resultado;
+  const x = readline.questionInt("Digite um número inteiro: ");
+  const resultado = fatorial(x);
+  return resultado;
 }
 
 console.log(roda_fatorial());
+
